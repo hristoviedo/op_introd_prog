e194513a33ab76824a5c23832206fabeed71028b
Hristo S. Oviedo <hristo.oviedohn@gmail.com>
2022-09-05 23:29:19 -0600

    feat(examples)Crear el archivo HolaMundo.java

---
+++ holaMundo/HolaMundo.java
@@ -0,0 +1,14 @@
+/*
+ * Programa de que muestra 
+ * el mensaje 'Hola mundo' 
+ * en consola.
+ * 
+ * Creador: Hristo S. Oviedo
+ * 
+*/
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
