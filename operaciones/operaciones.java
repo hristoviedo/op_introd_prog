9eaa59c4fe5a4787e8c28790a423bf179e81698f
Hristo S. Oviedo <hristo.oviedohn@gmail.com>
2022-09-05 23:54:26 -0600

    feat(examples) Crear el archivo operaciones.java

---
+++ operaciones/operaciones.java
@@ -0,0 +1,10 @@
+public class operaciones {
+
+    public static void main(String[] args) {
+        int resultado = 0;
+
+        resultado = 29 + 31;
+
+        System.out.println(resultado);
+    }
+}
