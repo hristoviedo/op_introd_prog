39926267d1f92951773822bffb685acfd3682454
Hristo S. Oviedo <hristo.oviedohn@gmail.com>
2022-09-05 23:59:14 -0600

    feat(examples) Agregar la función Sumar()

--- operaciones/operaciones.java
+++ operaciones/operaciones.java
@@ -3,8 +3,12 @@ public class operaciones {
     public static void main(String[] args) {
         int resultado = 0;
 
-        resultado = 29 + 31;
+        resultado = Sumar(16, 24);
 
         System.out.println(resultado);
+    }
+
+    public static int Sumar(int num1, int num2) {
+        return num1 + num2;
     }
 }
